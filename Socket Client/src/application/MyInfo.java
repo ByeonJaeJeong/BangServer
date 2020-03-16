0b8d4f55cff849697338bb8eff97f4bcc2fc3e8f
ByeonJaeJeong <qsz78547@naver.com>
2020-03-16 17:43:53 +0900

    2020-03-16

---
+++ Socket Client/src/application/MyInfo.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.net.Socket;
+
+public class MyInfo {
+	public static boolean socketConnect=false;
+	public static Socket socket=null;
+	
+	public static void setConnect(boolean state){
+		socketConnect=state;
+	}
+	public static void setSocket(Socket s){
+		socket=s;
+	}
+}
